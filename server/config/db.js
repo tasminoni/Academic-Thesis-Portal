77b8ba759001f432a2c789b49a3e3576438ca489
Tasmin Ahmed Oni <116836209+tasminoni@users.noreply.github.com>
2025-08-25 03:56:24 +0600

    new

--- server/config/db.js
+++ server/config/db.js
@@ -1,17 +1,13 @@
 import mongoose from "mongoose";
 
 // Try different connection strings
-const CONNECTION_URLS = [
-  "mongodb+srv://tasminahmedoni909:tasminoni12@cluster0.ifbev34.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-  "mongodb+srv://tasminahmedoni909:tasminoni12@cluster0.ifbev34.mongodb.net/academic_thesis_portal?retryWrites=true&w=majority",
-  "mongodb://localhost:27017/academic_thesis_portal" 
-];
+
 
 export const connectDB = async () => {
-  for (let i = 0; i < CONNECTION_URLS.length; i++) {
+
     try {
  
-      await mongoose.connect(CONNECTION_URLS[i], {
+      await mongoose.connect(process.env.MONGODB_URI, {
         serverSelectionTimeoutMS: 10000, // Timeout after 10s
         socketTimeoutMS: 45000, // Close sockets after 45s of inactivity
         family: 4 // Use IPv4, skip trying IPv6
@@ -23,6 +19,6 @@ export const connectDB = async () => {
         console.log("All connection attempts failed. Continuing without database connection...");
       }
     }
-  }
+  
 };
 
